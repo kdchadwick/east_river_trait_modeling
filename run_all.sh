a0c376b286cb6019ec0e60b4c028f3740ad9d53c
pbrodrick <pbrodrick@carnegiescience.edu>
2020-02-16 15:28:30 -0800

    added BIL mosaic extraction code to this repository

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@
 
 # run the extraction to get the NEON AOP data coincident with each ground location
 # This repository does not hold the specific file references for the below raster files - they can, however, be extracted from google earth engine - see manuscript for details
-python extract.py data/crown_raster.tif  data/site_spectra_extracted.csv -crown_shape_file data/CRBU2018_AOP_Crowns_centroid_extended.geojson -source_files ../../mosaic_2019/built_mosaic/min_phase_tch_me ../../mosaic_2019/built_mosaic/min_phase_wtrl ../../mosaic_2019/built_mosaic/min_phase_wtrv ../../mosaic_2019/built_mosaic/min_phase_obs ../../mosaic_2019/built_mosaic/min_phase_shade_centered ../../mosaic_2019/built_mosaic/min_phase_shade_tch ../../mosaic_2019/built_mosaic/min_phase_refl
+python extract_aop_data_from_mosaics.py data/crown_raster.tif  data/site_spectra_extracted.csv -crown_shape_file data/CRBU2018_AOP_Crowns_centroid_extended.geojson -source_files ../../mosaic_2019/built_mosaic/min_phase_tch_me ../../mosaic_2019/built_mosaic/min_phase_wtrl ../../mosaic_2019/built_mosaic/min_phase_wtrv ../../mosaic_2019/built_mosaic/min_phase_obs ../../mosaic_2019/built_mosaic/min_phase_shade_centered ../../mosaic_2019/built_mosaic/min_phase_shade_tch ../../mosaic_2019/built_mosaic/min_phase_refl
 
 # merge NEON AOP data extractions with trait data, and set up (and run) PLSR models under various conditions
 python merge_data_run_iterative_plsr.py PLSR/test CRBU_settings.txt -shade_type dsm 
