d7dd387b383cc5516a3784e3738c2ee4f2af218d
K. Dana Chadwick <kdchadwick@gmail.com>
2021-07-02 14:51:10 -0500

    updating acorn run files

--- atm_model_comparisons/am_acorn/run_acorn.sh
+++ atm_model_comparisons/am_acorn/run_acorn.sh
@@ -2,7 +2,7 @@
 # These are the code commands used to generate the PLSR models for atmospheric modeling method comparisons
 
 # merge NEON AOP data extractions with trait data, and set up (and run) PLSR models under various conditions
-python code/merge_data_run_iterative_plsr.py atm_model_comparisons/am_acorn atm_model_comparisons/am_acorn/CRBU_settings.txt -shade_type dsm -n_folds_to_run 10 -spectra_file data/spectra/20200214_CRBU2018_AOP_Crowns_extraction.csv
+python code/merge_data_run_iterative_plsr.py atm_model_comparisons/am_acorn atm_model_comparisons/am_acorn/CRBU_settings.txt -shade_type dsm -n_folds_to_run 10 -spectra_file data/spectra/acorn_refl_extractions.csv
 
 # run plotting code to create final output plots
 python code/plot_10fold_plsr_results.py -folds 10 -results_directory atm_model_comparisons/am_acorn -file_prefix atm_model_comparisons_am_acorn
